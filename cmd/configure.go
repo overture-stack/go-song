0e7d4257c54230024bdbc0ac65de0e00c6c9f10f
Dušan Andrić <dusan.andric@oicr.on.ca>
2017-09-06 11:39:58 -0400

    added some basic configuration logic

---
+++ cmd/configure.go
@@ -0,0 +1,75 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(configureCmd)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func verifyPath(fullPath string) {
+	_, err := os.Stat(fullPath)
+	if os.IsNotExist(err) {
+		fmt.Println("No configuration existing configuration file, creating new config.")
+	} else {
+		fmt.Println("Existing configuration found. Type 'y' to continue...")
+		var input string
+		fmt.Scanln(&input)
+		if input != "y" {
+			os.Exit(0)
+		}
+	}
+}
+
+func doConfigure() {
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fullPath := path.Join(home, ".song.yaml")
+	verifyPath(fullPath)
+
+	file, err := os.Create(fullPath)
+	check(err)
+	defer file.Close()
+
+	var accessToken string
+	fmt.Println("Please enter your access token: ")
+	fmt.Scanln(&accessToken)
+
+	var songURL string
+	fmt.Println("Please enter URL of SONG server: ")
+	fmt.Scanln(&songURL)
+
+	accessTokenConfig := "accessToken: " + accessToken + "\n"
+	songURLConfig := "songURL: " + songURL + "\n"
+
+	_, err = file.WriteString(accessTokenConfig)
+	check(err)
+	_, err = file.WriteString(songURLConfig)
+	check(err)
+	file.Sync()
+}
+
+var configureCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "configures SONG client",
+	Long:  `Sets configuration values in config file.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		doConfigure()
+	},
+}
--- cmd/root.go
+++ cmd/root.go
@@ -4,18 +4,24 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/fatih/color"
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 var cfgFile string
+var accessTokenFlag string
+var songURLFlag string
+
+var configErrorMsg = `Fatal error, could not find appropriate configuration values.
+Please use the configure command, --config option, or --accessToken and --songURL`
 
 // RootCmd is the Base Command for CLI Application
 var RootCmd = &cobra.Command{
 	Use:   `song`,
 	Short: `CLI Utility for uploading metadata to a SONG repository`,
-	Long:  `CLI Utitlity for uploading metadata to a SONG repository`,
+	Long:  `CLI Utility for uploading metadata to a SONG repository`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
 	},
@@ -30,6 +36,17 @@ func Execute() {
 	}
 }
 
+// VerifyConfig verifies if the required configuration values have been set.
+func VerifyConfig() {
+	accessToken := viper.Get("accessToken")
+	songURL := viper.Get("songURL")
+
+	if accessToken == nil || accessToken == "" || songURL == nil || songURL == "" {
+		color.Red("Fatal Error. Missing configuration values.")
+		color.Red("See --help for additional information.")
+	}
+}
+
 func init() {
 	cobra.OnInitialize(initConfig)
 
@@ -37,6 +54,8 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "use a config file (default is $HOME/.song.yaml)")
+	RootCmd.PersistentFlags().StringVar(&accessTokenFlag, "accessToken", "", "Provide an access token for authorizing operations to SONG")
+	RootCmd.PersistentFlags().StringVar(&songURLFlag, "songURL", "", "url of SONG server")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -45,6 +64,7 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
+
 	if cfgFile != "" {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
@@ -56,15 +76,20 @@ func initConfig() {
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".test-app" (without extension).
+		// Search config in home directory with name ".song" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigName(".song")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	viper.AutomaticEnv() // Read in environment variables that match
+	viper.ReadInConfig() // If a config file is found, read it in.
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	// CLI Overrides
+	if accessTokenFlag != "" {
+		viper.Set("accessToken", accessTokenFlag)
 	}
+	if songURLFlag != "" {
+		viper.Set("songURL", songURLFlag)
+	}
+
 }
